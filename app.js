dc119b857ee38a331141996e665c09430ac504e2
Joey O'bute <admin@Joeys-MacBook-Pro.local>
2020-10-27 17:18:37 +0000

    changed the message on line 5 for full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('When will my life begin?'));
+app.get('/', (req, res) => res.send('Who has the final say???'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
