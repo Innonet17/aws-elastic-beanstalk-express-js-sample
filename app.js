a77de1e539e48864d65fcfff7c850d2d69ff9158
Joey O'bute <admin@Joeys-MacBook-Pro.local>
2020-10-29 07:01:33 +0000

    testing more than one line update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('WHAT DO YOU REALLY WANT???'));
+app.get('/', (req, res) => res.send('WHEN THE GOING GETS TOUGH, THE TOUGH GETS GOING. '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
